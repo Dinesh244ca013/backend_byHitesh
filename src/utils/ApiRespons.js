9d3fb6aa6bb64bcb78aa47f89abfc00136474537
Dinesh244ca013 <dinesh100100mit@gmail.com>
2026-07-03 09:11:21 +0530

    util code discussion

---
+++ src/utils/ApiRespons.js
@@ -0,0 +1,12 @@
+class ApiRespons{
+   
+    constructor(statusCode , data , message = "Success"){
+      this.statusCode=statusCode
+      this.data = data
+      this.message = message 
+      this.success = statusCode 
+
+    }
+}
+
+export {ApiRespons}
